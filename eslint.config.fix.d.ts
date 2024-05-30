2290b23bbb170e4d1e0182550a7ce49393821a4a
Michael Naumov <mnaoumov@gmail.com>
2024-05-30 10:07:35 -0600

    Improve eslint typings

--- eslint.config.fix.d.ts
+++ eslint.config.fix.d.ts
@@ -16,15 +16,31 @@ declare module "@typescript-eslint/eslint-plugin" {
 }
 
 declare module "eslint-plugin-import" {
+  import type {
+    ESLint
+  } from "eslint";
+
+  const plugin: ESLint.Plugin;
+  export default plugin;
 }
 
 declare module "eslint-plugin-modules-newlines" {
+  import type {
+    ESLint
+  } from "eslint";
+
+  const plugin: ESLint.Plugin;
+  export default plugin;
 }
 
 declare module "globals" {
+  interface Globals {
+    [name: string]: boolean | "writable" | "readonly" | "off";
+  }
+
   const globals: {
-    browser: object;
-    node: object;
+    browser: Globals;
+    node: Globals;
   };
   export default globals;
 }
