9bbf547694d5d9598bc08ddb7b23426fd4c0458d
Michael Naumov <mnaoumov@gmail.com>
2024-05-31 00:00:11 -0600

    List artifacts without wildcards

--- scripts/npm/postversion.ts
+++ scripts/npm/postversion.ts
@@ -1,15 +1,20 @@
+import { readdir } from "fs/promises";
 import process from "process";
 import {
   execFromRoot,
   resolvePathFromRoot
 } from "scripts/tools/root.ts";
 
-export default function postversion(): void {
+export default async function postversion(): Promise<void> {
   execFromRoot("git push");
   execFromRoot("git push --tags");
 
   const newVersion = process.env["npm_package_version"];
 
   const buildDir = resolvePathFromRoot("dist/build");
-  execFromRoot(`gh release create "${newVersion}" "${buildDir}/*" --title "v${newVersion}" --generate-notes`);
+  const fileNames = await readdir(buildDir);
+  const filePaths = fileNames.map(fileName => `${buildDir}/${fileName}`);
+  const filePathsStr = filePaths.map(filePath => `"${filePath}"`).join(" ");
+
+  execFromRoot(`gh release create "${newVersion}" ${filePathsStr} --title "v${newVersion}" --generate-notes`);
 }
