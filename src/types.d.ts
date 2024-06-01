49d327839acf1b3bee5e28bf08fdd05f1fabca81
Michael Naumov <mnaoumov@gmail.com>
2024-06-01 13:53:42 -0600

    Avoid unnecessary casting

--- src/types.d.ts
+++
@@ -1,9 +0,0 @@
-// TODO: Remove when https://github.com/Fevol/obsidian-typings/pull/60/ is merged
-
-export { };
-
-declare module "obsidian" {
-  interface InternalPlugin {
-    enable(): Promise<void>;
-  }
-}
